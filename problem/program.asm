4308a6c977bdfd7910af39f23c30dacf16485a5a
Esteban Munoz Sepulveda <83989904+Peluhi@users.noreply.github.com>
2022-02-12 22:20:19 -0500

    pantalla completo negra y sin error

--- problem/program.asm
+++ problem/program.asm
@@ -2,17 +2,17 @@
 // COLOCA 000431110
 // COLOCA esteban.munozs@upb.edu.co
 //Comprobando estado, Primer programa sin hacer que cada opcion funcione con f o  c.
-//arr=16384 y como llega hasta ROM[24575] entonces n=24577-16384 resultando n=8193, entonces
+//arr=16384 y como llega hasta ROM[24575] entonces n=24575-16384 resultando n=8193, entonces
 @16384
 D=A
 @arr
 M=D
 //inicializo arr en 16384
-@8193
+@8192
 D=A
 @n
 M=D
-//inicializo n en 8193
+//inicializo n en 8192
 @i
 M=0
 //inicializo i en 0
