2d04e43b8913a7e2324507e3b80c0c8f14b33350
Esteban Munoz Sepulveda <83989904+Peluhi@users.noreply.github.com>
2022-02-13 02:07:41 -0500

    pantalla negra con f

--- problem/program.asm
+++ problem/program.asm
@@ -3,6 +3,17 @@
 // COLOCA esteban.munozs@upb.edu.co
 //Comprobando estado, Primer programa sin hacer que cada opcion funcione con f o  c.
 //arr=16384 y como llega hasta ROM[24575] entonces n=24575-16384 resultando n=8193, entonces
+(TECLAF)
+@KBD
+D=M
+@70
+D=D-A
+@NEGRO
+D;JEQ
+@TECLAF
+0;JMP
+
+(NEGRO)
 @16384
 D=A
 @arr
@@ -34,7 +45,7 @@ M=-1
 M=M+1
 @LOOP
 0;JMP
-//
+
 (END)
 @END
 0;JMP
