b85bf386856280a0cd03f470409a389ce81ea4ef
Esteban Munoz Sepulveda <83989904+Peluhi@users.noreply.github.com>
2022-02-13 02:31:00 -0500

    Programa funcional con F y C

--- problem/program.asm
+++ problem/program.asm
@@ -1,7 +1,7 @@
 // COLOCO estebanmunozsepulveda
 // COLOCA 000431110
 // COLOCA esteban.munozs@upb.edu.co
-//Comprobando estado, Primer programa sin hacer que cada opcion funcione con f o  c.
+//Comprobando estado, prgrama funcional con f y c.
 //arr=16384 y como llega hasta ROM[24575] entonces n=24575-16384 resultando n=8193, entonces
 (TECLAF)
 @KBD
@@ -27,12 +27,12 @@ M=D
 @i
 M=0
 //inicializo i en 0
-(LOOP)
+(LOOP1)
 @i
 D=M
 @n
 D=D-M
-@END
+@TECLAC
 D;JEQ
 //
 @arr
@@ -43,9 +43,48 @@ M=-1
 //
 @i
 M=M+1
-@LOOP
+@LOOP1
 0;JMP
 
-(END)
-@END
+(TECLAC)
+@KBD
+D=M
+@67
+D=D-A
+@BLANCO
+D;JEQ
+@TECLAC
+0;JMP
+
+(BLANCO)
+@16384
+D=A
+@arr
+M=D
+//inicializo arr en 16384
+@8192
+D=A
+@n
+M=D
+//inicializo n en 8192
+@i
+M=0
+//inicializo i en 0
+(LOOP2)
+@i
+D=M
+@n
+D=D-M
+@TECLAF
+D;JEQ
+//
+@arr
+D=M
+@i
+A=D+M
+M=0
+//
+@i
+M=M+1
+@LOOP2
 0;JMP
